117b4320c5a55ba6982fd5e3d0d54b4d2db5578f
Nightloewe <nightloewe@gmail.com>
2021-05-19 19:18:18 +0200

    Fixed lib.rs Session structs, currently implementing rocket.rs session types

--- src/lib.rs
+++ src/lib.rs
@@ -2,90 +2,78 @@ use chrono::{DateTime, Utc};
 use std::any::Any;
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex, RwLock};
-use rocket::fairing::{Fairing, Info, Kind};
-use rocket::Rocket;
 use std::borrow::Borrow;
 use erased_serde::Serialize;
 
-pub mod cookie;
-
-pub trait Session {
-    /// Returns the id of the session
-    fn id(&self) -> &str;
-
-    /// Sets the id of the session
-    ///
-    /// # Arguments
-    ///
-    /// * `new_id` - String slice for new id
-    ///
-    fn set_id(&mut self, new_id: &str);
-
-    /// Returns the creation time of the session
-    fn creation_time(&self) -> &DateTime<Utc>;
-
-    /// Returns the time the user last accessed using the session
-    fn last_accessed(&self) -> &DateTime<Utc>;
-
-    /// Returns the time the session will expire or
-    /// [`chrono::MIN_DATETIME`] if the session never expires
-    fn expires(&self) -> &DateTime<Utc>;
-
-    /// Returns true if the session is expired
-    fn is_expired(&self) -> bool;
-
-    /// Updates the last accessed and expiration time
-    fn update_time(&mut self);
-
-    /// Inserts a new attribute to the session
-    ///
-    /// # Arguments
-    ///
-    /// * `key` - Unique string key for the attribute
-    /// * `attribute` - the attribute
-    ///
-    fn insert<T>(&mut self, key: &str, attribute: T) where T: Serialize + Any;
-
-    /// Removes an attribute by its key and returns
-    /// the removed attribute if there was one
-    ///
-    /// # Arguments
-    ///
-    /// * `key` - The key of the attribute to remove
-    ///
-    fn remove(&mut self, key: &str) -> Option<Box<dyn Serialize>>;
-
-    /// Returns the attribute behind the given key
-    ///
-    /// # Arguments
-    ///
-    /// * `key` - The key of the attribute to return
-    ///
-    fn get_attribute<T>(&self, key: &str) -> Option<&T> where T: Serialize + Any;
+/// Trait to upcast Attribute to Any and use [`std::any::Any.downcast_ref(&self)`]
+pub trait AsAny {
+    fn as_any_ref(&self) -> &dyn Any;
 }
 
-pub trait SessionConfig: Send + Sync {
+/// AsAny is implemented for all structs implementing Any
+impl<T: Any> AsAny for T {
+    fn as_any_ref(&self) -> &dyn Any {
+        self
+    }
+}
+
+/// Attribute is the type representing data in a session. It requires the struct to implement Any,
+/// Serialize, Send and Sync.
+trait Attribute: AsAny + Serialize + Send + Sync + 'static {}
+
+/// Attribute is automatically implemented for the named traits
+impl<T> Attribute for T where T: AsAny + Serialize + Send + Sync + 'static {}
+
+///A session that can be created by the framework in case it needs session data
+pub trait Session {
+    /// Accesses the session data behind this session and acquires the lock protecting the data
+    /// of the session.
+    ///
+    /// All access, mutable and immutable shall be done in this method.
+    ///
+    /// # Arguments
+    ///
+    /// * `closure` - The method to access the session data
+    ///
+    fn access<F: FnMut(&SessionInner)>(closure: F);
+
+    /// Invalidates the session by setting expiration date to [`Session.creation_time()`]
+    fn invalidate(&mut self);
+}
+
+/// Configuration for sessions, such as duration until the session expires
+pub trait SessionConfig: Send + Sync + 'static {
     /// Returns the duration until a session expires
     fn expiration_duration(&self) -> Option<chrono::Duration>;
+
+    /// Returns an id generator for ids of the session
+    fn id_gen(&self) -> &dyn Fn() -> String;
 }
 
-pub struct SessionInner<'a> {
+/// Struct holding the real session data
+pub struct SessionInner {
     id: String,
     created: DateTime<Utc>,
     last_accessed: DateTime<Utc>,
     expires: DateTime<Utc>,
-    attributes: HashMap<String, Box<dyn Any>>,
-    _config: &'a dyn SessionConfig
+    attributes: HashMap<String, Box<dyn Attribute>>,
+    _config: &'static dyn SessionConfig
 }
 
-impl<'a> SessionInner<'a> {
-    pub fn new(id: &str, config: &'a dyn SessionConfig) -> SessionInner<'a> {
+impl SessionInner {
+    /// Creates a new [`SessionInner`] with the given id and session config
+    ///
+    /// # Arguments
+    ///
+    /// * `id` - The id of the session
+    /// * `config` - The session config for this session type
+    pub fn new(id: &str, config: &'static dyn SessionConfig) -> SessionInner {
         let creation_time = Utc::now();
 
         SessionInner {
             id: id.to_string(),
             created: creation_time,
-            last_accessed: creation_time,
+            last_accessed: creation_time.clone(),
             expires: match config.expiration_duration() {
                 Some(duration) => {
                     creation_time + duration
@@ -96,29 +84,39 @@ impl<'a> SessionInner<'a> {
             _config: config
         }
     }
-}
 
-impl<'a> Session for SessionInner<'a> {
+    /// Returns the id of the session
     fn id(&self) -> &str {
         self.id.as_str()
     }
 
+    /// Sets the id of the session
+    ///
+    /// # Arguments
+    ///
+    /// * `new_id` - String slice for new id
+    ///
     fn set_id(&mut self, new_id: &str) {
         self.id = new_id.to_string();
     }
 
+    /// Returns the creation time of the session
     fn creation_time(&self) -> &DateTime<Utc> {
         &self.created
     }
 
+    /// Returns the time the user last accessed using the session
     fn last_accessed(&self) -> &DateTime<Utc> {
         &self.last_accessed
     }
 
+    /// Returns the time the session will expire or
+    /// [`chrono::MIN_DATETIME`] if the session never expires
     fn expires(&self) -> &DateTime<Utc> {
         &self.expires
     }
 
+    /// Returns true if the session is expired
     fn is_expired(&self) -> bool {
         let current_time = Utc::now();
 
@@ -129,6 +127,7 @@ impl<'a> Session for SessionInner<'a> {
         }
     }
 
+    /// Updates the last accessed and expiration time
     fn update_time(&mut self) {
         self.last_accessed = Utc::now();
 
@@ -137,22 +136,52 @@ impl<'a> Session for SessionInner<'a> {
         }
     }
 
-    fn insert<T>(&mut self, key: &str, attribute: T)
-        where T: Serialize + Any {
+    /// Invalidates the session by setting expiration date to [`Session.creation_time()`]
+    fn invalidate(&mut self) {
+        self.expires = self.created.clone();
+    }
+
+    /// Inserts a new attribute to the session
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - Unique string key for the attribute
+    /// * `attribute` - the attribute
+    ///
+    fn insert<T:>(&mut self, key: &str, attribute: T)
+        where T: Attribute {
         self.attributes.insert(key.to_string(), Box::new(attribute));
     }
 
-    fn remove(&mut self, key: &str) -> Option<Box<dyn Any>> {
+    /// Removes an attribute by its key and returns
+    /// the removed attribute if there was one
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - The key of the attribute to remove
+    ///
+    fn remove(&mut self, key: &str) -> Option<Box<dyn Attribute>> {
         self.attributes.remove(key)
     }
 
+    /// Returns the attribute behind the given key
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - The key of the attribute to return
+    ///
     fn get_attribute<T>(&self, key: &str) -> Option<&T>
-        where T: Serialize + Any {
+        where T: Attribute {
         let result = self.attributes.get(key);
 
         match result {
             Some(data) => {
-                return data.downcast_ref()
+                let attr = data.as_any_ref();
+                if attr.is::<T>() {
+                    return attr.downcast_ref::<T>();
+                } else {
+                    None
+                }
             },
             None => None
         }
@@ -160,9 +189,11 @@ impl<'a> Session for SessionInner<'a> {
 }
 
 /// A map containing all sessions as [`SessionData`]
-pub trait SessionMap: Send + Sync {
+///
+/// The map chooses where to save session data and retrieve it, e.g. in RAM or Redis
+pub trait SessionMap: Send + Sync + 'static {
     /// Lists all sessions in this SessionMap
-    fn list(&self) -> Vec<&SessionData>;
+    fn list(&self, closure: &dyn Fn(Vec<&SessionData>));
 
     /// Gets a session by its id
     ///
@@ -170,7 +201,7 @@ pub trait SessionMap: Send + Sync {
     ///
     /// * `id` - A string slice containing the id
     ///
-    fn get(&self, id: &str) -> Option<&SessionData>;
+    fn get(&self, id: &str) -> Option<SessionData>;
 
     /// Inserts a session into the map
     ///
@@ -199,36 +230,43 @@ pub trait SessionMap: Send + Sync {
     fn clear(&self);
 }
 
-type SessionData<'a> = Arc<Mutex<SessionInner<'a>>>;
+/// The type to protect session data from data races
+type SessionData = Arc<Mutex<SessionInner>>;
 
-pub struct LocalSessionMap<'a> {
-    sessions: RwLock<HashMap<String, SessionData<'a>>>
+/// A session map to save session data in RAM
+pub struct LocalSessionMap {
+    sessions: RwLock<HashMap<String, SessionData>>
 }
 
-impl<'a> LocalSessionMap<'a> {
-    pub fn new() -> LocalSessionMap<'a> {
+impl LocalSessionMap {
+    pub fn new() -> LocalSessionMap {
         LocalSessionMap {
             sessions: RwLock::new(HashMap::new())
         }
     }
 }
 
-impl<'a> SessionMap for LocalSessionMap<'a> {
-    fn list(&self) -> Vec<&SessionData<'a>> {
+impl SessionMap for LocalSessionMap {
+    fn list(&self, closure: &dyn Fn(Vec<&SessionData>)) {
         match self.sessions.read() {
-            Ok(map) => map.values().collect(),
+            Ok(map) => closure(map.values().collect()),
             Err(e) => panic!("Unable to acquire session map lock, {}", e)
         }
     }
 
-    fn get(&self, id: &str) -> Option<&SessionData<'a>> {
+    fn get(&self, id: &str) -> Option<SessionData> {
         match self.sessions.read() {
-            Ok(map) => map.get(id),
+            Ok(map) => {
+                if map.contains_key(id) {
+                    return Some(map.get(id).unwrap().clone());
+                }
+                None
+            },
             Err(e) => panic!("Unable to acquire session map lock: {}", e)
         }
     }
 
-    fn insert(&self, id: &str, session: SessionData<'a>) {
+    fn insert(&self, id: &str, session: SessionData) {
         match self.sessions.write() {
             Ok(mut map) => map.insert(id.to_string(), session),
             Err(e) => panic!("Unable to acqurie session map write lock: {}", e)
@@ -252,17 +290,18 @@ impl<'a> SessionMap for LocalSessionMap<'a> {
 
         match self.sessions.write() {
             Ok(mut map) => {
-                for val in map.values() {
-                    match val.lock() {
-                        Ok(data) => {
-                            if data.is_expired() {
-                                map.remove(data.id());
+                map.retain(|key, data| -> bool {
+                    match data.lock() {
+                        Ok(inner) => {
+                            if inner.is_expired() {
                                 removed += 1;
+                                return true;
                             }
-                        }
-                        Err(err) => todo!()
+                            return false
+                        },
+                        Err(err) => false
                     }
-                }
+                });
             },
             Err(e) => panic!("Unable to acqurie session map write lock: {}", e)
         };
@@ -273,45 +312,77 @@ impl<'a> SessionMap for LocalSessionMap<'a> {
     fn clear(&self) {
         match self.sessions.write() {
             Ok(mut map) => {
-                for val in map.values() {
-                    match val.try_lock() {
-                        Ok(data) => map.remove(data.id()),
-                        Err(err) => todo!()
-                    }
-                }
+                map.retain(|key, val| {
+                    match val.lock() {
+                        Ok(mut data) => data.invalidate(),
+                        Err(err) => todo!(),
+                    };
+                    false
+                })
             },
             Err(e) => panic!("Unable to acquire session map write lock: {}", e)
         };
     }
 }
 
+/// Session Store is the public interface for the framework to access sessions.
 struct SessionStore {
     inner: Box<dyn SessionMap>
 }
 
 impl SessionStore {
+    /// Creates a new session store
+    ///
+    /// # Arguments
+    ///
+    /// * `map` - The session map used by the store
+    ///
+    pub fn new<T: SessionMap>(map: T) -> SessionStore {
+        SessionStore {
+            inner: Box::new(map),
+        }
+    }
     /// Lists all sessions in this SessionStore
-    fn list(&self) -> Vec<&SessionData> {
-        self.inner.borrow().list()
+    fn list<F: Fn(Vec<&SessionData>)>(&self, closure: F)  {
+        let map: &dyn SessionMap = self.inner.borrow();
+        map.list(&closure);
     }
 
     /// Gets a session by its id
+    ///
+    /// The implementation of [`From<SessionData>`] gets passed a clone of [`SessionData`].
     ///
     /// # Arguments
     ///
     /// * `id` - A string slice containing the id
     ///
-    fn get<'a, T>(&self, id: &str) -> Option<T> where T: Session + Clone + From<&'a SessionData<'a>> {
-        let session_data = self.inner.borrow().get(id);
+    fn get<'a, T>(&'a self, id: &str) -> Option<T> where T: Session + Clone + From<SessionData> {
+        let map: &'a dyn SessionMap = self.inner.borrow();
+        let session_data = map.get(id);
 
         if let Some(data) = session_data {
-            Some(T::from(data))
+            match data.lock() {
+                Ok(inner) => {
+                    if inner.is_expired() {
+                        return None;
+                    }
+
+                    Some(T::from(data.clone()))
+                }
+                Err(err) => {
+                    /// TODO: add error message
+                    /// error!("Removing session {} from session map due to {}", id, err)
+                    None
+                }
+            }
         } else {
             None
         }
     }
 
-    /// Saves a session to the store
+    /// Saves a session to the store.
+    ///
+    /// Inside of this method a clone of the SessionData occurs.
     ///
     /// # Arguments
     ///
@@ -319,10 +390,11 @@ impl SessionStore {
     /// * `session` - A struct implementing the [`Session`], [`Clone`] and [`Into<SessionData>`]
     ///               traits
     ///
-    fn insert<'a, T>(&self, id: &str, session: T) where T: Session + Clone + Into<&'a SessionData<'a>> {
+    fn insert<'a, T>(&self, id: &str, session: &'a T) where T: Session + Clone + Into<&'a SessionData> {
         let session_data: &SessionData = session.into();
 
-        self.inner.borrow().insert(id, session_data.clone())
+        let map: &dyn SessionMap = self.inner.borrow();
+        map.insert(id, session_data.clone())
     }
 
     /// Removes a session by its id and returns true if there was a session removed
@@ -332,12 +404,14 @@ impl SessionStore {
     /// * `id` - The unique id of the session
     ///
     fn remove(&self, id: &str) -> bool {
-        self.inner.borrow().remove(id)
+        let map: &dyn SessionMap = self.inner.borrow();
+        map.remove(id)
     }
 
     /// Removes expired sessions and returns the count of expired sessions
     fn remove_expired(&self) -> usize {
-        self.inner.borrow().remove_expired()
+        let map: &dyn SessionMap = self.inner.borrow();
+        map.remove_expired()
     }
 
     /// Clears the session store, invalidating all sessions.
@@ -345,70 +419,22 @@ impl SessionStore {
     /// The underlying map needs to call invalidate on all sessions before
     /// removing them.
     fn clear(&self) {
-        self.inner.borrow().clear();
+        let map: &dyn SessionMap = self.inner.borrow();
+        map.clear();
     }
 }
 
-///
-///
-/// Configuration for fairing
-///
-///
-
-
-pub struct Sessions {
-    store: SessionStore,
-    configs: Vec<Box<dyn SessionConfig>>
-}
-
-impl Sessions {
-    fn new() -> Sessions {
-        Sessions {
-            store: SessionStore::new(LocalSessionMap::new()),
-            configs: vec![]
-        }
-    }
-
-    fn config<T: SessionConfig>(mut self, config: T) -> Sessions {
-        self.configs.push(Box::new(config));
-
-        self
-    }
-
-    fn session_map<T: SessionMap>(mut self, map: T) -> Sessions {
-        self.store.inner = map;
-
-        self
-    }
-
-    fn fairing(self) -> SessionFairing {
-        SessionFairing { inner: self }
+impl Default for SessionStore {
+    fn default() -> Self {
+        SessionStore::new(LocalSessionMap::new())
     }
 }
 
-///
-///
-/// Fairing
-///
-///
+#[cfg(feature = "rocket")]
+mod rocket;
 
-pub struct SessionFairing {
-    inner: Sessions
-}
-
-impl Fairing for SessionFairing {
-    fn info(&self) -> Info {
-        Info {
-            name: "Sessions",
-            kind: Kind::Attach
-        }
-    }
-
-    fn on_attach(&self, rocket: Rocket) -> Result<Rocket, Rocket> {
-        //add session store to managed state as well as all session configs
-        todo!()
-    }
-}
+#[cfg(feature = "rocket")]
+pub use crate::rocket::*;
 
 #[cfg(test)]
 mod tests {
---
+++ src/rocket.rs
@@ -0,0 +1,214 @@
+use crate::{SessionStore, SessionConfig, LocalSessionMap, SessionMap, SessionData, SessionInner};
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::{Rocket, Request, State};
+use std::mem;
+use std::borrow::BorrowMut;
+use std::cell::RefCell;
+use std::sync::Mutex;
+use std::ops::DerefMut;
+use rocket::request::{FromRequest, Outcome};
+use rocket::http::{Status, Cookie};
+use chrono::Duration;
+use rand::{thread_rng, Rng};
+use rand::distributions::Alphanumeric;
+
+struct NoSessionError;
+
+pub struct CookieSessionConfig {
+    cookie_name: String,
+    expiration: Duration,
+    _id_gen: Box<dyn Fn() -> String + Send + Sync>,
+}
+
+impl CookieSessionConfig {
+    pub fn new<F>(cookie_name: &str, expiration_duration: Duration, id_gen: F) -> CookieSessionConfig
+    where F: Fn() -> String + Send + Sync {
+        CookieSessionConfig {
+            cookie_name: cookie_name.to_string(),
+            expiration: expiration_duration,
+            _id_gen: Box::new(id_gen)
+        }
+    }
+
+    pub fn cookie_name(&self) -> &str {
+         self.cookie_name.as_str()
+    }
+}
+
+impl SessionConfig for CookieSessionConfig {
+    fn expiration_duration(&self) -> Option<Duration> {
+        if self.expiration.is_zero() {
+            None
+        } else {
+            Some(self.expiration.clone())
+        }
+    }
+
+    fn id_gen(&self) -> &dyn Fn() -> String {
+        return self._id_gen.as_ref()
+    }
+}
+
+impl Default for CookieSessionConfig {
+    fn default() -> Self {
+        CookieSessionConfig {
+            cookie_name: "session".to_string(),
+            expiration: Duration::hours(1),
+            _id_gen: Box::new(|| -> String {
+                thread_rng().sample_iter(&Alphanumeric).take(24).map(char::from).collect::<String>()
+            })
+        }
+    }
+}
+
+///
+///
+/// CookieSession
+///
+///
+pub struct CookieSession {
+    data: SessionData
+}
+
+impl FromRequest<'_, '_> for CookieSession {
+    type Error = NoSessionError;
+
+    fn from_request(request: &Request) -> Outcome<Self, Self::Error> {
+        let cookie_config = request.guard::<State<CookieSessionConfig>>()?;
+        let session_store = request.guard::<State<SessionStore>>()?;
+
+        //What needs to be implemented is:
+        //1. CookieSessionConfig with cookie name </
+        //2. Retrieve CookieSessionConfig as Request Guard </
+        //3. Check for cookie with cookie name and retrieve it </
+        //4a. If Cookie is None => create new session, save it and send cookie </
+        //4b. If Cookie is Some => retrieve it, get session </
+        //4ba. If Session is Some => return it </
+        //4ba. If Session is None => delete cookie and get back to from_request </
+
+        match request.cookies().get(cookie_config.cookie_name()) {
+            None => {
+                let generate = cookie_config.id_gen();
+                let id: String = generate();
+
+                let session = CookieSession {
+                    data: SessionData::new(Mutex::new(SessionInner::new(id.as_str(), &*cookie_config))),
+                };
+
+                session_store.insert(id.as_str(), session.clone());
+
+                Outcome::Success(session)
+            },
+            Some(cookie) => {
+                let session = session_store.get::<CookieSession>(cookie.value());
+
+                match session {
+                    Some(cookie_session) => {
+                        Outcome::Success(cookie_session)
+                    },
+                    None => {
+                        request.cookies().remove(Cookie::named(cookie.name()));
+
+                        Self::from_request(request)
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl From<SessionData> for CookieSession {
+    fn from(data: SessionData) -> Self {
+        CookieSession {
+            data
+        }
+    }
+}
+
+impl<'a> From<&'a CookieSession> for &'a SessionData {
+    fn from(session: &'a CookieSession) -> Self {
+        &session.data
+    }
+}
+
+impl Clone for CookieSession {
+    fn clone(&self) -> Self {
+        CookieSession {
+            data: SessionData::clone(&self.data)
+        }
+    }
+
+    fn clone_from(&mut self, source: &Self) {
+        self.data = SessionData::clone(&source.data)
+    }
+}
+
+///
+///
+/// Configuration for fairing
+///
+///
+
+
+pub struct Sessions {
+    store: SessionStore,
+    configs: Vec<Box<dyn SessionConfig>>
+}
+
+impl Sessions {
+    fn new() -> Sessions {
+        Sessions {
+            store: SessionStore::new(LocalSessionMap::new()),
+            configs: vec![]
+        }
+    }
+
+    fn config<T: SessionConfig>(mut self, config: T) -> Sessions {
+        self.configs.push(Box::new(config));
+
+        self
+    }
+
+    fn session_map<T: SessionMap>(mut self, map: T) -> Sessions {
+        self.store.inner = Box::new(map);
+
+        self
+    }
+
+    fn fairing(self) -> SessionFairing {
+        SessionFairing { inner: Mutex::new(self) }
+    }
+}
+
+///
+///
+/// Fairing
+///
+///
+
+pub struct SessionFairing {
+    inner: Mutex<Sessions>
+}
+
+impl Fairing for SessionFairing {
+    fn info(&self) -> Info {
+        Info {
+            name: "Sessions",
+            kind: Kind::Attach
+        }
+    }
+
+    fn on_attach(&self, rocket: Rocket) -> Result<Rocket, Rocket> {
+        //add session store to managed state as well as all session configs
+        let mut sessions = self.inner.lock().unwrap();
+        let store = mem::take(&mut sessions.store);
+
+        let mut rocket = rocket.manage(store);
+
+        for i in 0..sessions.configs.len() {
+            rocket = rocket.manage(sessions.configs.remove(i));
+        }
+
+        Ok(rocket)
+    }
+}
